83360a67fbbbfcc2afac2182d5c0f46064428b8a
EddPorter <github@eddporter.com>
2013-06-07 11:41:21 +0100

    Add 4x supersampling

--- RayManCpp/Scene.cpp
+++ RayManCpp/Scene.cpp
@@ -53,88 +53,95 @@ namespace rayman {
     for (unsigned y = 0; y < myScene.sizey; ++y) {
       for (unsigned x = 0; x < myScene.sizex; ++x) {
         float blue(0), red(0), green(0);
-        float coef(1.0f);
-        unsigned iter = 0;
 
-        // Cast the ray.
-        // There is no natural starting point (due to us using orthographic projection)
-        // so arbitrarily put it 1000.0f behind the "centre" of the scene.
-        ray viewRay = {{float(x), float(y), -1000.0f}, {0.0f, 0.0f, 1.0f}};
-        do {
-          float t = 2000.0f;
+        // Compute using 4x Super Sampling in a 2x2 grid.
+        for (float fragmentx = x; fragmentx < x + 1.0f; fragmentx += 0.5f) {
+          for (float fragmenty = y; fragmenty < y + 1.0f; fragmenty += 0.5f) {
+            // Each sample contributes 0.25 to the main pixel.
+            float coef(0.25f);
+            unsigned iter = 0;
 
-          // For each sphere
-          const sphere * hits = nullptr;
-          for (auto s = myScene.sphereContainer.cbegin(); s != myScene.sphereContainer.cend(); ++s) {
-            if (hitSphere(viewRay, *s, t)) {
-              hits = &(*s);
-            }
-          }
-          if (hits == nullptr) {
-            break;
-          }
+            // Cast the ray.
+            // There is no natural starting point (due to us using orthographic projection)
+            // so arbitrarily put it 1000.0f behind the "centre" of the scene.
+            ray viewRay = {{float(fragmentx), float(fragmenty), -1000.0f}, {0.0f, 0.0f, 1.0f}};
+            do {
+              float t = 2000.0f;
 
-          // Calculate point of intersection.
-          point intersect = viewRay.start + t * viewRay.dir;
-
-          // Determine the normal on the sphere at the point of intersection.
-          vector norm = intersect - hits->pos;
-          float temp = norm * norm;
-          if (temp == 0.0f) {
-            break;
-          }
-          temp = 1.0f / sqrtf(temp);
-          vector normalisedNorm = temp * norm;
-
-          // Check if the incident light is pointing back at us by checking the dot product.
-          for (auto l = myScene.lightContainer.cbegin(); l != myScene.lightContainer.cend(); ++l) {
-            vector dist = l->pos - intersect;
-            if (normalisedNorm * dist <= 0.0f) {
-              continue;
-            }
-
-            float t = sqrtf(dist * dist);
-            if (t <= 0.0f) {
-              continue;
-            }
-            ray lightRay = {intersect, (1 / t) * dist};
-
-            // Are we in shadow?
-            bool inShadow = false;
-            for (auto s = myScene.sphereContainer.cbegin(); s != myScene.sphereContainer.cend(); ++s) {
-              if (hitSphere(lightRay, *s, t)) {
-                inShadow = true;
+              // For each sphere
+              const sphere * hits = nullptr;
+              for (auto s = myScene.sphereContainer.cbegin(); s != myScene.sphereContainer.cend(); ++s) {
+                if (hitSphere(viewRay, *s, t)) {
+                  hits = &(*s);
+                }
+              }
+              if (hits == nullptr) {
                 break;
               }
-            }
-            if (!inShadow) {
-              // Apply lambertian reflectance.
-              float lambert = (lightRay.dir * normalisedNorm) * coef;
-              red += lambert * l->red * hits->mat.red;
-              green += lambert * l->green * hits->mat.green;
-              blue += lambert * l->blue * hits->mat.blue;
 
-              // Apply Blinn-Phong reflection model.
-              vector blinnDir = lightRay.dir - viewRay.dir;
-              float temp = sqrtf(blinnDir * blinnDir);
-              if (temp != 0.0f) {
-                vector normalisedBlinnDir = (1.0f / temp) * blinnDir;
-                float blinnTerm = std::max(normalisedBlinnDir * normalisedNorm, 0.0f) ;
-                blinnTerm = hits->mat.specvalue * powf(blinnTerm, hits->mat.specpower) * coef;
-                red += blinnTerm * l->red;
-                green += blinnTerm * l->green;
-                blue += blinnTerm * l->blue;
+              // Calculate point of intersection.
+              point intersect = viewRay.start + t * viewRay.dir;
+
+              // Determine the normal on the sphere at the point of intersection.
+              vector norm = intersect - hits->pos;
+              float temp = norm * norm;
+              if (temp == 0.0f) {
+                break;
               }
-            }
+              temp = 1.0f / sqrtf(temp);
+              vector normalisedNorm = temp * norm;
+
+              // Check if the incident light is pointing back at us by checking the dot product.
+              for (auto l = myScene.lightContainer.cbegin(); l != myScene.lightContainer.cend(); ++l) {
+                vector dist = l->pos - intersect;
+                if (normalisedNorm * dist <= 0.0f) {
+                  continue;
+                }
+
+                float t = sqrtf(dist * dist);
+                if (t <= 0.0f) {
+                  continue;
+                }
+                ray lightRay = {intersect, (1 / t) * dist};
+
+                // Are we in shadow?
+                bool inShadow = false;
+                for (auto s = myScene.sphereContainer.cbegin(); s != myScene.sphereContainer.cend(); ++s) {
+                  if (hitSphere(lightRay, *s, t)) {
+                    inShadow = true;
+                    break;
+                  }
+                }
+                if (!inShadow) {
+                  // Apply lambertian reflectance.
+                  float lambert = (lightRay.dir * normalisedNorm) * coef;
+                  red += lambert * l->red * hits->mat.red;
+                  green += lambert * l->green * hits->mat.green;
+                  blue += lambert * l->blue * hits->mat.blue;
+
+                  // Apply Blinn-Phong reflection model.
+                  vector blinnDir = lightRay.dir - viewRay.dir;
+                  float temp = sqrtf(blinnDir * blinnDir);
+                  if (temp != 0.0f) {
+                    vector normalisedBlinnDir = (1.0f / temp) * blinnDir;
+                    float blinnTerm = std::max(normalisedBlinnDir * normalisedNorm, 0.0f) ;
+                    blinnTerm = hits->mat.specvalue * powf(blinnTerm, hits->mat.specpower) * coef;
+                    red += blinnTerm * l->red;
+                    green += blinnTerm * l->green;
+                    blue += blinnTerm * l->blue;
+                  }
+                }
+              }
+
+              // For reflections in the material.
+              coef *= hits->mat.reflection;
+              float reflect = 2.0f * (viewRay.dir * normalisedNorm);
+              viewRay.start = intersect;
+              viewRay.dir = viewRay.dir - reflect * normalisedNorm;
+
+            } while (coef > 0.0f && ++iter < 10);
           }
-
-          // For reflections in the material.
-          coef *= hits->mat.reflection;
-          float reflect = 2.0f * (viewRay.dir * normalisedNorm);
-          viewRay.start = intersect;
-          viewRay.dir = viewRay.dir - reflect * normalisedNorm;
-
-        } while (coef > 0.0f && ++iter < 10);
+        }
         imageFile.put((unsigned char)std::min(blue * 255.0f, 255.0f)).put((unsigned char)std::min(green * 255.0f, 255.0f)).put((unsigned char)std::min(red * 255.0f, 255.0f));
       }
     }
